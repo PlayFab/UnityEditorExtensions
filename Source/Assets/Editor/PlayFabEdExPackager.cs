11ad7a346bd864021b9d4c26429439f06a779037
pgilmorepf <paul@playfab.com>
2017-04-20 14:15:01 -0700

    The First-Time-Install-time errors that some people have been seeing should be cleared up.
    Summary:
    Specifically, the resources included in the UnityPackage don't seem to be available, despite the fact that code from the same package is running.
    So the code now skips rendering until those assets become available.
    
    Test Plan: Jenker!
    
    Reviewers: Marco.Williams
    
    Differential Revision: https://phab.playfabdev.com/D3638

--- Source/Assets/Editor/PlayFabEdExPackager.cs
+++ Source/Assets/Editor/PlayFabEdExPackager.cs
@@ -9,7 +9,7 @@ namespace PlayFab.Internal
         "Assets/PlayFabEditorExtensions"
     };
 
-        [MenuItem("PlayFab/Build PlayFab UnityPackage")]
+        [MenuItem("PlayFab/Build PlayFab EdEx UnityPackage")]
         public static void BuildUnityPackage()
         {
             var packagePath = "C:/depot/sdks/UnityEditorExtensions/Packages/PlayFabEditorExtensions.unitypackage";
