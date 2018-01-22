242f99b1f9f9f92f4fdc1583a2aa50cd797b6e6b
PlayFabJenkinsBot <playfabjenkinsbot@github.com>
2018-01-22 22:53:35 +0000

    https://api.playfab.com/releaseNotes/#180122

--- Source/Assets/PlayFabEditorExtensions/Editor/Scripts/Utils/PlayFabEditorVersion.cs
+++ Source/Assets/PlayFabEditorExtensions/Editor/Scripts/Utils/PlayFabEditorVersion.cs
@@ -1,1 +1,1 @@
-namespace PlayFab.PfEditor { public static partial class PlayFabEditorHelper { public static string EDEX_VERSION = "2.34.180122"; } }
+namespace PlayFab.PfEditor { public static partial class PlayFabEditorHelper { public static string EDEX_VERSION = "2.35.180122"; } }
